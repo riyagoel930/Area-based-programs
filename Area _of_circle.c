436e7e12fe4f9d32fd31d7aa49e42441bdc21953
riyagoel930 <113978413+riyagoel930@users.noreply.github.com>
2022-10-10 20:23:00 +0530

    Create Area _of_circle.c

---
+++ Area _of_circle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int num1,num,sum,radi;
+printf("Enter  radius: ");
+scanf("%d",&radi);
+
+int area = 3.14*radi*radi;
+printf("Area of circle is %d",area);
+return 0;
+}
+
+
+Output:
+
+
+Enter  radius: 3
+Area of circle is 28
